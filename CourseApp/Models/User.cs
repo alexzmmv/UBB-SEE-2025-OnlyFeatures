2a264904b4c87a78213efa4c0d71e723df044f9a
Sonia Ferent <sonia.ferent@stud.ubbcluj.ro>
2025-04-02 19:05:36 +0300

    timer-logic

--- CourseApp/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CourseApp.Models
-{
-    public class User
-    {
-        public int UserId { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
-    }
-}
