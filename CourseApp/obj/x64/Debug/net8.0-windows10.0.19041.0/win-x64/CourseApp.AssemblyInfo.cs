f0479ad4685ddff94554b39b72c959e2d3ea2c7f
Sonia Ferent <sonia.ferent@stud.ubbcluj.ro>
2025-04-03 09:20:47 +0300

    roadmap courses

--- CourseApp/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/CourseApp.AssemblyInfo.cs
+++ CourseApp/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/CourseApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f1581a80032bfa515e1607fec4c1d490c10891f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d000a80124059b4668ae16961ce8ad33557a534b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
